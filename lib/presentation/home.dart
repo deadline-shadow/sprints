794fc6bec56e722cd3a9d0b4ea137574c1b09e33
JoeBiden <joebiden@gmail.com>
2025-03-04 21:09:52 +0400

    a

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -206,11 +206,11 @@ class _HomeState extends State<Home> {
                                       horizontal: 8.0,
                                     ),
                                     child: Container(
-                                      color: Colors.white,
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.circular(
                                           10.0,
                                         ),
+                                        color: Colors.white,
                                       ),
                                       child: Padding(
                                         padding: const EdgeInsets.all(16),
